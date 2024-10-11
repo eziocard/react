5ba6cd3a7b648286f72ce15819951ea1cd21bda9
eziocard <rjlopezjobs@gmail.com>
2024-10-10 22:21:42 -0300

    react

---
+++ react/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import TarjetaPersona from './components/TarjetaPersona.jsx'
+function App() {
+
+  return (
+    <>
+    <TarjetaPersona/>
+    </>
+  )
+}
+
+export default App
---
+++ react/src/components/TarjetaPersona.jsx
@@ -0,0 +1,7 @@
+function TarjetaPersona(){
+  return (
+    <button>hola</button>
+  )
+}
+
+export default TarjetaPersona
